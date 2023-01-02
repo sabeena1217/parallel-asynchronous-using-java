1e5de1220e52d69c2831707da9b88eb93f80b2cd
sabeena1217 <sabeena1217@gmail.com>
2023-01-02 07:52:17 +0530

    13 - executorService/future and its limitations

---
+++ src/main/java/com/learnjava/executor/ProductServiceUsingExecutor.java
@@ -0,0 +1,50 @@
+package com.learnjava.executor;
+
+import com.learnjava.domain.Product;
+import com.learnjava.domain.ProductInfo;
+import com.learnjava.domain.Review;
+import com.learnjava.service.ProductInfoService;
+import com.learnjava.service.ReviewService;
+
+import java.util.concurrent.*;
+
+import static com.learnjava.util.CommonUtil.stopWatch;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class ProductServiceUsingExecutor {
+    private ProductInfoService productInfoService;
+    private ReviewService reviewService;
+    static ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public ProductServiceUsingExecutor(ProductInfoService productInfoService, ReviewService reviewService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public Product retrieveProductDetails(String productId) throws ExecutionException, InterruptedException, TimeoutException {
+        stopWatch.start();
+
+        Future<ProductInfo> productInfoFuture = executorService.submit(() -> productInfoService.retrieveProductInfo(productId));
+        Future<Review> reviewFuture = executorService.submit(() -> reviewService.retrieveReviews(productId));
+
+        ProductInfo productInfo = productInfoFuture.get(2, TimeUnit.SECONDS); // bocking the call of thread
+        Review review = reviewFuture.get();
+
+        stopWatch.stop();
+
+        log("Total Time Taken : "+ stopWatch.getTime());
+        return new Product(productId, productInfo, review);
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+
+        ProductInfoService productInfoService = new ProductInfoService();
+        ReviewService reviewService = new ReviewService();
+        ProductServiceUsingExecutor productService = new ProductServiceUsingExecutor(productInfoService, reviewService);
+        String productId = "ABC123";
+        Product product = productService.retrieveProductDetails(productId);
+        log("Product is " + product);
+
+        executorService.shutdown();
+    }
+}
