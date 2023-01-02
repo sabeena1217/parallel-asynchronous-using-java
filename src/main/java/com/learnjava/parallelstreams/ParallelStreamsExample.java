d22dddb5dea7382acf84e6973af4f5758a2081d3
sabeena1217 <sabeena1217@gmail.com>
2023-01-03 00:40:40 +0530

    17 - parallel streams

---
+++ src/main/java/com/learnjava/parallelstreams/ParallelStreamsExample.java
@@ -0,0 +1,34 @@
+package com.learnjava.parallelstreams;
+
+import com.learnjava.util.DataSet;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.learnjava.util.CommonUtil.*;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class ParallelStreamsExample {
+
+    List<String> stringTransform(List<String> namesList) {
+        return namesList
+//                .stream()
+                .parallelStream()
+                .map(this::addNameLengthTransform)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<String> namesList = DataSet.namesList();
+        ParallelStreamsExample parallelStreamsExample = new ParallelStreamsExample();
+        startTimer();
+        List<String> resultList = parallelStreamsExample.stringTransform(namesList);
+        log("resultList : " + resultList);
+        timeTaken();
+    }
+
+    private String addNameLengthTransform(String name) {
+        delay(500);
+        return name.length() + " - " + name;
+    }
+}
