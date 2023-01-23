f0c42b3abdab3338ee82957299a4ccb480167b19
sabeena1217 <sabeena1217@gmail.com>
2023-01-23 09:28:29 +0530

    36 - CompletableFuture

---
+++ src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
@@ -0,0 +1,22 @@
+package com.learnjava.completablefuture;
+
+import com.learnjava.service.HelloWorldService;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.learnjava.util.LoggerUtil.log;
+
+public class CompletableFutureHelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorldService hws = new HelloWorldService();
+        CompletableFuture.supplyAsync(() -> hws.helloWorld())
+                .thenAccept((result) -> {
+                    log("Result is " + result);
+                })
+                .join();
+
+        log("Done!");
+//        delay(2000);
+    }
+}
